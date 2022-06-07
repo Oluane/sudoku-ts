34f151581fd18ef7374930f3c57224064c96d36a
Oluane <rochepauline6@gmail.com>
2022-06-07 10:50:33 +0200

    adding a font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" />
+          <meta name="description" content="Let's play Sudoku!" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
